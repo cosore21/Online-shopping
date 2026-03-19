3a622a89a1b4077f26948d0679c35597a1568f92
clement <clementosore2020@gmail.com>
2026-03-19 17:22:43 +0300

    calculating cart Quantity

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,146 +1,8 @@
 
 
-const products = [{
-  image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-  name: 'Plain Cotton T-shirt',
-  rating: {
-    stars: 3,
-    count: 87
-  },
-  priceCents: 2089
-},
-{
-  image: 'images/products/intermediate-composite-basketball.jpg',
-  name: 'Composite BasketBall',
-  rating: {
-    stars: 4.5,
-    count: 120
-  },
-  priceCents: 1999
-},
-{
-  image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-  name: 'Athletic Cotton Socks',
-  rating: {
-    stars: 4,
-    count: 100
-  },
-  priceCents: 2321
-},
-{
-  image: 'images/products/umbrella.jpg',
-  name: 'Pocket Umbrella',
-  rating: {
-    stars: 4,
-    count: 110
-  },
-  priceCents: 321
-},
-{
-  image: 'images/products/backpack.jpg',
-  name: 'Fashion Backpack',
-  rating: {
-    stars: 4,
-    count: 110
-  },
-  priceCents: 821
-},
-{
-  image: 'images/products/duvet-cover-set-blue-twin.jpg',
-  name: 'Duvet Cover Set Blue',
-  rating: {
-    stars: 5,
-    count: 98
-  },
-  priceCents: 8321
-},
-{
-  image: 'images/products/vanity-mirror-silver.jpg',
-  name: 'Vanity Mirror Silver',
-  rating: {
-    stars: 4,
-    count: 119
-  },
-  priceCents: 321
-},
-{
-  image: 'images/products/sky-flower-stud-earrings.webp',
-  name: 'Sky Flower Earings',
-  rating: {
-    stars: 4,
-    count: 101
-  },
-  priceCents: 200
-},
-{
-  image: 'images/products/plain-hooded-fleece-sweatshirt-yellow.jpg',
-  name: 'Hooded Fleece Sweatshirt',
-  rating: {
-    stars: 5,
-    count: 121
-  },
-  priceCents: 3321
-},
-{
-  image: 'images/products/blackout-curtain-set-beige.webp',
-  name: 'Blackout Curtain',
-  rating: {
-    stars: 4,
-    count: 99
-  },
-  priceCents: 4009
-},
-{
-  image: 'images/products/round-sunglasses-black.jpg',
-  name: 'Black Sunglasses',
-  rating: {
-    stars: 4,
-    count: 110
-  },
-  priceCents: 921
-},
-{
-  image: 'images/products/straw-sunhat.webp',
-  name: 'Sunhat',
-  rating: {
-    stars: 4,
-    count: 117
-  },
-  priceCents: 291
-},
-{
-  image: 'images/products/women-chiffon-beachwear-coverup-black.jpg',
-  name: 'Women Beachwear',
-  rating: {
-    stars: 4.5,
-    count: 77
-  },
-  priceCents: 1091
-},
-{
-  image: 'images/products/women-french-terry-fleece-jogger-camo.jpg',
-  name: 'Terry Fleece Jogger',
-  rating: {
-    stars: 4.5,
-    count: 70
-  },
-  priceCents: 1020
-},
-{
-  image: 'images/products/women-knit-ballet-flat-black.jpg',
-  name: 'Women Knit Ballet',
-  rating: {
-    stars: 4.5,
-    count: 70
-  },
-  priceCents: 2020
-},
-]
-
-
-let productHTML=''
+let productHTML = ''
 products.forEach((product) => {
-   productHTML += `
+  productHTML += `
   
    <div class="product-container">
           <div class="product-image-container">
@@ -161,7 +23,7 @@ products.forEach((product) => {
           </div>
 
           <div class="product-price">
-          $${(product.priceCents / 100)}
+          $${(product.priceCents / 100).toFixed(2)}
           </div>
 
           <div class="product-quantity-container">
@@ -186,12 +48,40 @@ products.forEach((product) => {
             Added
           </div>
 
-          <button class="add-to-cart-button button-primary">
+          <button class="js-add-to-cart add-to-cart-button button-primary "data-product-id="${product.id}">
             Add to Cart
           </button>
         </div>
   
   `
+
   document.querySelector('.js-products-grid').innerHTML = productHTML
 
+
 });
+
+
+
+document.querySelectorAll('.js-add-to-cart').forEach((button) => {
+  button.addEventListener('click', () => {
+    const productId = button.dataset.productId;
+
+
+    let matchingItem;
+    cart.forEach((item) => {
+      if (productId === item.productId) {
+        matchingItem = item
+      }
+    })
+    if (matchingItem) {
+      matchingItem.quantity += 1
+    } else {
+      cart.push({
+        productId: productId,
+        quantity: 1
+      })
+    }
+    console.log(cart);
+
+  })
+})
